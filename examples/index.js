570e9a21c116a40e77805405cffcadf28bb481b1
Jonathan Carter <joncart@outlook.com>
2017-04-28 14:32:13 -0700

    Correcting example

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 const { login } = require("../");
 const { ResourceManagementClient } = require("azure-arm-resource");
 
-login({ userAgent: "example" }).then(({ clientFactory }) => {
+login().then(({ clientFactory }) => {
     const { resourceGroups } = clientFactory(ResourceManagementClient);
     return resourceGroups.list();
 }).then((groups) => console.log(groups));
